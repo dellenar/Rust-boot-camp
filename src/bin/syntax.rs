722406e4b1a971782f73a751dd7179228195046a
Dellenar <ozinanberkan@gmail.com>
2023-11-10 17:12:12 +0300

    First commit

---
+++ src/bin/syntax.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let message = "Hello World"; // String variable
+
+    let x: i32 = 42; // Number variable
+
+    let pi : f64 = 3.14; // float type
+
+    let is_rust_fun: bool = true; // boolean type
+
+    let letter_a: char = 'a'; // char type
+
+    fn add(x: i32, y:i32) -> i32 {
+        return x + y; // or can be used without return like -> x + y  
+    } // to create function do fn name()
+
+    let x: i32 = 4;
+
+    if x >= 4{
+        println!("x is not negative");
+    }else{
+        println!("x is negative");
+    }
+
+    let mut i: i32  = 1;
+
+    while i <= 5 {
+        println!("{}", i);
+        i += 1;
+    }
+
+}
